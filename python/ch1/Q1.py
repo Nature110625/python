a4c9e6aed8bb52c1bbc879f191dfcc286ec8cae8
PRAKRITI <136888456+Nature110625@users.noreply.github.com>
2024-02-06 16:15:58 +0530

    ..

--- python/ch1/Q1.py
+++
@@ -1,5 +0,0 @@
-#Write a program to print Twinkle Twinkle little star poem in python
-print('Twinkle Twinkle little star')
-print('How I wonder what you are')
-print('Up above the world so high')
-print('Like a diamond in the sky')
